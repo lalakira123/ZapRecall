14ff744653303d030fa697da5781603abdffe1e0
Laerte <laerteakira@gmail.com>
2022-03-18 14:35:12 -0300

    Exibição do Resultado

--- src/components/TelaDois.js
+++ src/components/TelaDois.js
@@ -7,9 +7,43 @@ const tags = [];
 function TelaDois(props) {
     const [totalRespondido, setRespondido] = React.useState(0);
     const [selecionado, setSelecionado] = React.useState(<></>);
+    const [resultado, setResultado] = React.useState(<></>);
+    const [escondido, setEscondido] = React.useState("");
 
     function atualizarTelaDois(valor) {
         setRespondido(totalRespondido + valor);
+    }
+
+    function verificarResultado() {
+        if (totalRespondido === 7 && tags.filter((e) => e.alt === "x").length > 0) {
+            setEscondido("escondido");
+            setResultado(
+                <div className="resultado">
+                    <div className="emoji">
+                        <img src="./image/sad.png"alt="sad"/>
+                        <p>PUTZ!</p>
+                    </div>
+                    <div className="mensagem">
+                        <p>Ainda faltaram alguns ...</p>
+                        <p>Mas não desanime!</p>
+                    </div>
+                </div>
+            );
+        } else if ( totalRespondido === 7) {
+            setEscondido("escondido");
+            setResultado(
+                <div className="resultado">
+                    <div className="emoji">
+                        <img src="./image/party.png" alt="party"/>
+                        <p>PARABÉNS!</p>
+                    </div>
+                    <div className="mensagem">
+                        <p>Você não esqueceu de </p>
+                        <p>nenhum flashcard!</p>
+                    </div>
+                </div>
+            );
+        }
     }
 
     function acrescentarIcone(icone) {
@@ -18,23 +52,21 @@ function TelaDois(props) {
                 img:"./image/x.png",
                 alt:"x"
             });
-            console.log(tags);
         }
         if (icone === "selecionadoVerde") {
             tags.push({
                 img:"./image/check.png",
                 alt:"check"
             });  
-            console.log(tags);
         }
         if (icone === "selecionadoAmarelo") {
             tags.push({
                 img:"./image/interrogation.png",
                 alt:"interrogation"
-            });
-            console.log(tags); 
+            }); 
         }
         setSelecionado(tags.map((tag, index) => <img key={index} src={tag.img} alt={tag.alt}/>));
+        verificarResultado();
     }
 
     return (
@@ -48,7 +80,8 @@ function TelaDois(props) {
                 <Cards atualizar={atualizarTelaDois} acrescentar={acrescentarIcone}/>
 
                 <footer>
-                    <p>{totalRespondido}/8 CONCLUÍDOS</p>
+                    {resultado}
+                    <p className={escondido}>{totalRespondido}/8 CONCLUÍDOS</p>
                     <div>
                         {selecionado}
                     </div>
